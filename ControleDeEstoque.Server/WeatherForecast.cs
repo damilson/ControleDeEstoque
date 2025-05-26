4f28e0d7619fc25dbb8942553a04c32a8fd109e0
Matheus Lima <omatheusdasilvalima@gmail.com>
2025-05-26 12:50:38 -0300

    dei um enter na linha

--- ControleDeEstoque.Server/WeatherForecast.cs
+++ ControleDeEstoque.Server/WeatherForecast.cs
@@ -4,6 +4,7 @@ namespace ControleDeEstoque.Server
     {
         public DateOnly Date { get; set; }
 
+
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
